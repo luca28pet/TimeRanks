a7d49c103f91f7ee70ce7393b4259095b10b78eb
luca28pet <lucapetrucci28@yahoo.it>
2015-01-13 12:10:41 +0100

    Spaces.

--- src/TimeRanks/minuteSchedule.php
+++ src/TimeRanks/minuteSchedule.php
@@ -8,14 +8,14 @@ use pocketmine\utils\Config;
 
 class minuteSchedule extends PluginTask{
   
-  public function __construct($plugin){
-    $this->plugin = $plugin;
-	  parent::__construct($plugin);
-  }
+	public function __construct($plugin){
+		$this->plugin = $plugin;
+		parent::__construct($plugin);
+	}
 
 	public function onRun($currentTick){
-		foreach($this->getServer()->getOnlinePlayers() as $p){
-	  	if(!($this->plugin->times->exists($p))){
+		foreach($this->plugin->getServer()->getOnlinePlayers() as $p){
+	  		if(!($this->plugin->times->exists($p))){
 				$this->plugin->times->set($p, array(1));
 			}else{
 				$currentminute = $this->plugin->times->get($p[0]) + 1;
