b92aa06fa6742dee9a80b71c9037bba9381909e1
luca28pet <lucapetrucci28@yahoo.it>
2015-07-05 22:14:10 +0200

    Fix for command

--- src/TimeRanks/TimeRanksCommand.php
+++ src/TimeRanks/TimeRanksCommand.php
@@ -20,6 +20,20 @@ class TimeRanksCommand{
         $sub = array_shift($args);
         switch(strtolower($sub)){
             case "check":
+                if(isset($args[0])){
+                    if(!$this->plugin->getServer()->getOfflinePlayer($args[0])->hasPlayedBefore()){
+                        $sender->sendMessage("Player ".$args[0]." never played on this server");
+                    }else{
+                        if(!isset($this->plugin->data[strtolower($args[0])])){
+                            $sender->sendMessage($args[0]." has played less than 1 minute on this server");
+                            $sender->sendMessage("Rank is: ".$this->plugin->getRank($args[0]));
+                        }else{
+                            $sender->sendMessage($args[0]." has played ".$this->plugin->data[strtolower($sender->getName())]["minutes"]." minutes on this server");
+                            $sender->sendMessage("Rank is: ".$this->plugin->getRank($args[0]));
+                        }
+                    }
+                    return true;
+                }
                 if(!isset($this->plugin->data[strtolower($sender->getName())])){
                     if(!($sender instanceof Player)){
                         $sender->sendMessage("Please use /tr check <playername>");
