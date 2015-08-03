191df6f6c56dc55eb3b25356aee80449d4e2dfba
luca28pet <lucapetrucci28@yahoo.it>
2015-08-03 19:27:37 +0200

    Optimizations

--- src/TimeRanks/Timer.php
+++ src/TimeRanks/Timer.php
@@ -13,8 +13,7 @@ class Timer extends PluginTask{
 
     public function onRun($tick){
         foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
-            $name = strtolower($player->getName());
-            $this->plugin->data->exists($name) ? $this->plugin->data->set($name, (int) $this->plugin->data->get($name) + 1) : $this->plugin->data->set($name, 1);
+            $this->plugin->data->exists($name = strtolower($player->getName())) ? $this->plugin->data->set($name, (int) $this->plugin->data->get($name) + 1) : $this->plugin->data->set($name, 1);
             $this->plugin->data->save();
             $this->plugin->checkRank($player);
         }
