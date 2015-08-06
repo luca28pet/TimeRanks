3df0df7cc1e4491046b127fe936c6ff4c4131f7c
luca28pet <lucapetrucci28@yahoo.it>
2015-08-06 15:55:56 +0200

    Updated events

--- src/TimeRanks/events/PlayerRankUpEvent.php
+++ src/TimeRanks/events/PlayerRankUpEvent.php
@@ -3,18 +3,27 @@
 namespace TimeRanks\events;
 
 use pocketmine\event\Cancellable;
-use pocketmine\event\player\PlayerEvent;
+use pocketmine\event\plugin\PluginEvent;
 use pocketmine\Player;
+use TimeRanks\Main;
 
-class PlayerRankUpEvent extends PlayerEvent implements Cancellable{
+class PlayerRankUpEvent extends PluginEvent implements Cancellable{
 
+    public static $handlerList = null;
+
+    private $player;
     private $newRank;
     private $message;
 
-    public function __construct(Player $player, $newRank, $message){
+    public function __construct(Main $plugin, Player $player, $newRank, $message){
+        parent::__construct($plugin);
         $this->player = $player;
         $this->newRank = $newRank;
         $this->message = $message;
+    }
+
+    public function getPlayer(){
+        return $this->player;
     }
 
     public function getNewRank(){
