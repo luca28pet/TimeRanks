1eed21df387cfa74a10840ae80fd93f41ae9cff2
BuildTools <mattwattz@gmail.com>
2016-01-03 13:50:09 +0000

    Block users from mins increasing when not logged in

--- src/TimeRanks/task/Timer.php
+++ src/TimeRanks/task/Timer.php
@@ -4,26 +4,41 @@ namespace TimeRanks\task;
 
 use pocketmine\scheduler\PluginTask;
 use TimeRanks\TimeRanks;
+use SimpleAuth\SimpleAuth;
 
 class Timer extends PluginTask{
-
+    /**
+     * @var TimeRanks $timeRanks TimeRanks plugins
+     */
     private $timeRanks;
+    
+    /**
+     *
+     * @var SimpleAuth
+     */
+    private $simpleAuth;
 
     public function __construct(TimeRanks $timeRanks){
         parent::__construct($timeRanks);
         $this->timeRanks = $timeRanks;
+        $this->simpleAuth = $timeRanks->getServer()->getPluginManager()->getPlugin("SimpleAuth");
     }
 
     public function onRun($tick){
-		$this->timeRanks->dataprovider->start_transaction();
+        $this->timeRanks->dataprovider->start_transaction();
         foreach($this->timeRanks->getServer()->getOnlinePlayers() as $p){
-            if(!$p->hasPermission("timeranks.exempt")){
+            if( ! $this->simpleAuth->isPlayerAuthenticated($p) ) {
+                $msg = "Blocked " . $p->getName() . " from timerank minute up ";
+                $msg .= "as they are not authenticated";
+                $this->timeRanks->getServer()->getLogger()->debug($msg);
+                continue;
+            } elseif(!$p->hasPermission("timeranks.exempt")){
                 $this->timeRanks->setMinutes($p->getName(), $this->timeRanks->getMinutes($p->getName()) + 1, true);
             } elseif($this->timeRanks->trackAllPlayersTimes) {
-				$this->timeRanks->setMinutes($p->getName(), $this->timeRanks->getMinutes($p->getName()) + 1, false);
-			}
+                $this->timeRanks->setMinutes($p->getName(), $this->timeRanks->getMinutes($p->getName()) + 1, false);
+            }
         }
-		$this->timeRanks->dataprovider->commit_transaction();
+	$this->timeRanks->dataprovider->commit_transaction();
     }
 
 }
